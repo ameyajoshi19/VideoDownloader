486f4a8f9e1ae810afdd7266dacc50a00a334794
Ameya Joshi <joshi.amey@husky.neu.edu>
2015-02-20 21:10:36 -0800

    Initial commit

---
+++ src/com/downloader/video/VideoDownloader.java
@@ -0,0 +1,19 @@
+package com.downloader.video;
+
+import java.io.IOException;
+
+/**
+ * <p>
+ * This is the main class for the VideoDownloader project. Given a URL link, this app will scrap that
+ * page for all URLs and pick out the video links found in the video. It will then download these
+ * videos.
+ * </p>
+ * @author AmeyaCJoshi
+ *
+ */
+public class VideoDownloader {
+
+	public static void main(String args[]) throws IOException {
+		
+	}
+}

3efb73620f30a578869520a0ba64928a121ac244
Ameya Joshi <ameyajoshi19@gmail.com>
2015-02-21 18:10:27 -0800

    implemented flow of the app.

--- src/com/downloader/video/VideoDownloader.java
+++ src/com/downloader/video/VideoDownloader.java
@@ -3,15 +3,17 @@ package com.downloader.video;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import com.downloader.video.util.VideoDownloderUtility;
 
 /**
  * <p>
- * This is the main class for the VideoDownloader project. Given a URL link, this app will scrap that
+ * This is the main class for the Video Downloader app. Given a URL link, this app will scrape the
  * page for all URLs and pick out the video links found in the video. It will then download these
  * videos.
  * </p>
+ * 
  * @author AmeyaCJoshi
  *
  */
@@ -20,16 +22,21 @@ public class VideoDownloader {
 	public static void main(String args[]) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
+		VideoDownloader downloader = new VideoDownloader();
 		String pageUrl = br.readLine();
-		
-		startProcessing(pageUrl);
+		downloader.startProcessing(pageUrl);
 	}
 	
-	private static void startProcessing(String URL) {
+	private void startProcessing(String URL) {
 		VideoDownloderUtility util = new VideoDownloderUtility();
+		
+		// Check if the given string is a valid URL.
 		boolean isUrlValid = util.isUrlValid(URL);
 		if(isUrlValid) {
-			//TODO: process URL.
+			List<String> linksFoundInThePage = util.parseWebPageToFindAllHrefs(URL);
+			List<String> videoHrefsFoundInThePage = util.findAllVideoLinks(linksFoundInThePage);
+			util.downloadVideos(videoHrefsFoundInThePage);
+			
 		} else {
 			System.err.println(String.format("Malformed URL! {%s} is not a valid URL.", URL));
 		}
--- src/com/downloader/video/util/VideoDownloderUtility.java
+++ src/com/downloader/video/util/VideoDownloderUtility.java
@@ -1,15 +1,39 @@
 package com.downloader.video.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.validator.routines.UrlValidator;
 
 /**
  * <p>
  * This class contains the utility methods required by the video downloader app.
  * </p>
+ * 
  * @author AmeyaCJoshi
  *
  */
 public class VideoDownloderUtility {
+	
+	public void downloadVideos(List<String> videoLinks) {
+		//TODO: implement
+	}
+	
+	public List<String> findAllVideoLinks(List<String> allLinks) {
+		List<String> allVideoLinks = new ArrayList<String>();
+		
+		//TODO: implement
+		
+		return allVideoLinks;
+	}
+	
+	public List<String> parseWebPageToFindAllHrefs(String URL) {
+		List<String> hrefsInThePage = new ArrayList<String>();
+		
+		//TODO: implement
+		
+		return hrefsInThePage;
+	}
 	
 	public boolean isUrlValid(String URL) {
 		UrlValidator validator = new UrlValidator();
